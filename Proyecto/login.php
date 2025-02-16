f7ebabdab190a0877a1f76d278434645a354c495
Ovando Jesús Adrián <130518598+adrianOvando@users.noreply.github.com>
2025-02-15 23:26:17 -0400

    15/2

---
+++ Proyecto/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="read.php" method="post">
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email">
+        <label for="contraseña">Contraseña</label>
+        <input type="password" name="contraseña" id="contraseña"><br><br>
+        <button type="submit">Iniciar Seción</button>
+    </form>
+
+    
+</body>
+</html>
