b2509c57fd3a5b33109dc26439f326cad426e0e9
Ovando Jesús Adrián <130518598+adrianOvando@users.noreply.github.com>
2025-02-18 21:41:05 -0400

    18-02

--- Proyecto/login.php
+++ Proyecto/login.php
@@ -3,17 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="log"> 
     <form action="read.php" method="post">
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email">
-        <label for="contraseña">Contraseña</label>
-        <input type="password" name="contraseña" id="contraseña"><br><br>
-        <button type="submit">Iniciar Seción</button>
+        <a style="display: flex; justify-content: center;">Login</a><br><br>
+        <label for="email" id="L">Email</label><br><br>
+        <input type="text" id="email" name="email"><br><br>
+        <label for="contraseña" id="L">Contraseña</label><br><br>
+        <input type="password" name="contraseña" id="contraseña"><br><br><br>
+        <div class="bt">
+        <button type="submit" id="button">Iniciar Seción</button>
+        <div>
     </form>
-
+    </div>
     
 </body>
 </html>
